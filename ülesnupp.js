1c30aa61367101366657c5fd622efa39b2309da2
Daria Savtšenko <dariasav@ut.ee>
2023-12-04 16:35:39 +0200

    Muudsin muutujate ja failide nimed

--- ülesnupp.js
+++ ülesnupp.js
@@ -1,23 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
-    var rootElement = document.documentElement;
+// Samuti, nagu küsimustiga, ma leidsin internetis paar näited ja hakkasin suhtlema Chat GPT-ga, et muuta neid oma lehe jaoks.
+// Näite internetist, mida ma kasutasin: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
 
+// sündmuskuulaja, mis ootab, kuni kogu HTML-dokument on laaditud
+document.addEventListener("DOMContentLoaded", function () {
+    var keriülesNupp = document.getElementById("keriülesNupp");
+    var rootElement = document.documentElement;
+    
+    // funktsioon, mis käivitatakse iga kord, kui lehekülge keritakse
     function handleScroll() {
         if (rootElement.scrollTop > 100) {
-            scrollToTopBtn.style.display = "block";
+            keriülesNupp.style.display = "block";
         } else {
-            scrollToTopBtn.style.display = "none";
+            keriülesNupp.style.display = "none";
         }
     }
 
+    // funktsioon, mis viib lehekülje ülaosale sujuva liikumisega
     function scrollToTop() {
         rootElement.scrollTo({
             top: 0,
             behavior: "smooth"
         });
     }
-
+    // sündmuskuulaja lisamine, mis reageerib kerimisele, käivitades funktsiooni "handleScroll"
     window.addEventListener("scroll", handleScroll);
-
-    scrollToTopBtn.addEventListener("click", scrollToTop);
+    // sündmuskuulaja lisamine, mis reageerib nupule "scrollToTopBtn" klõpsamisele, käivitades funktsiooni "scrollToTop"
+    keriülesNupp.addEventListener("click", scrollToTop);
 });
